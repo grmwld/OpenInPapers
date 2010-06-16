35ee92ed5dd10b48f67b3975c5f9a867e522dc4d
ymazuz <ymazuz@mac.com>
2010-06-16 13:49:11 +0800

    Contextual menu event handler to check for link and selection.

--- OpenInPapers.safariextension/inject.js
+++ OpenInPapers.safariextension/inject.js
@@ -1,5 +1,7 @@
 window.addEventListener('keydown', checkKeyDown, false);
 window.addEventListener('keyup', checkKeyUp, false);
+document.addEventListener("contextmenu", getContextMenuEvent, false);
+
 
 function checkKeyDown(event)
 {
@@ -19,11 +21,26 @@ function checkKeyDown(event)
     }
 }
 
-function checkKeyUp(event)
-{
-    // <ctrl> key release
-	if(event.keyCode == 17)
-	{
-		safari.self.tab.dispatchMessage('ctrlStatusUpdate', false);
-	}
+
+
+function getContextMenuEvent(event) {
+	
+	selection = event.view.document.getSelection() + ""; // get page selection as string 
+	selection = selection.replace(/^\s+|\s+$/g,""); // strip leading and trailing whitespace
+
+	// don't need to pass messages, since the global script can see this event
+	// we can just add a userInfo object to it. 
+	var userInfo = new Object();
+
+
+ 	userInfo.hasSelection = (sel.length != 0);   
+	userInfo.selection = userInfo.hasSelection ? selection : "";
+
+	// does the right-click target have an HREF attribute?
+	userInfo.hasLink = !(typeof event.target.href == "undefined");
+	userInfo.href = userInfo.hasLink ? event.target.href : null;
+    
+	// add userInfo to the event so it'll be seen by the validate function 
+	safari.self.tab.setContextMenuEventUserInfo(event, userInfo);
 }
+
