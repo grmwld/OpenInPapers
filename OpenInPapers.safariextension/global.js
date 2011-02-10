0b3ac7457e7df37cc30ead6ced3e0670830361b2
Alexis Grimaldi <alexis.grimaldi+dev@gmail.com>
2011-02-10 23:15:23 +0100

    Keyboard shortcut specification. Closes [3].
    The key can be specified in the preferences.
    The key should be an lower case character.
    If it is not, the first character of the string is used.
    The keyboard shortcut is to be used along the static <ctrl> meta key.

--- OpenInPapers.safariextension/global.js
+++ OpenInPapers.safariextension/global.js
@@ -3,68 +3,57 @@ safari.application.addEventListener("validate", validateCommand, false);
 safari.application.addEventListener("message", handleMessage, false);
 
 
-function performCommand(event)
-{   
-    if (event.command === "openInPapers")
-    {
-        url = event.target.browserWindow.activeTab.url;
-        title = event.target.browserWindow.activeTab.title;
+function performCommand(event) {
+  if (event.command === "openInPapers") {
+    var url = event.target.browserWindow.activeTab.url;
+    var title = event.target.browserWindow.activeTab.title;
+  }
+  else if (event.command === "openInPapersMenu") {
+    var url = safari.application.activeBrowserWindow.activeTab.url;
+    var title = safari.application.activeBrowserWindow.activeTab.title;
+    if (event.userInfo) {
+      title = event.userInfo.selection;
     }
-    else if (event.command === "openInPapersMenu")
-    {
-        url = safari.application.activeBrowserWindow.activeTab.url;
-        title = safari.application.activeBrowserWindow.activeTab.title;
-        if (event.userInfo)
-        {
-            title = event.userInfo.selection;
-        }
+  }
+  else if (event.command === "openLinkInPapers") {
+    var url = event.userInfo.href;
+    var title = event.userInfo.selection;
+  }
+  openURLinPapers(url, title);
+}
+
+
+function openURLinPapers(url, title) {
+  var papersURL = 'papers://url/' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title);
+  safari.application.activeBrowserWindow.activeTab.url = papersURL;
+}
+
+
+function validateCommand(event) {
+  if (event.command === "openInPapers") {
+    event.target.disabled = !event.target.browserWindow.activeTab.url;
+  }
+  if (event.command === "openInPapersMenu") {
+    event.target.disabled = event.userInfo.hasLink;
+  }
+  if (event.command === "openLinkInPapers") {
+    event.target.disabled = !event.userInfo.hasLink;
+  }
+}
+
+
+function handleMessage(event) {
+  if (event.name == 'openInPapersViaKeyboardShortcut') {
+    url = event.target.browserWindow.activeTab.url;
+    title = event.target.browserWindow.activeTab.title;
+
+    if (event.message) {
+      title = event.message;
     }
-    else if (event.command === "openLinkInPapers")
-    {
-        url = event.userInfo.href;
-        title = event.userInfo.selection;
-    }
-    
     openURLinPapers(url, title);
-}
-
-
-function openURLinPapers(url, title)
-{
-    papersURL = 'papers://url/' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title);
-    safari.application.activeBrowserWindow.activeTab.url = papersURL;
-}
-
-
-function validateCommand(event)
-{
-    if (event.command === "openInPapers")
-    {
-        event.target.disabled = !event.target.browserWindow.activeTab.url;
-    }
-    if (event.command === "openInPapersMenu")
-    {
-        event.target.disabled = event.userInfo.hasLink;
-    }
-    if (event.command === "openLinkInPapers")
-    {
-        event.target.disabled = !event.userInfo.hasLink;
-    }
-}
-
-
-function handleMessage(event)
-{
-    if (event.name == 'openInPapersViaKeyboardShortcut')
-    {
-        url = event.target.browserWindow.activeTab.url;
-        title = event.target.browserWindow.activeTab.title;
-        
-        if (event.message)
-        {
-            title = event.message;
-        }
-        
-        openURLinPapers(url, title);
-    }
+  }
+  if (event.name == 'getSettingValue') {
+    var value = safari.extension.settings.getItem(event.message);
+    safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("settingValueIs", [event.message, value]);
+  }
 }
--- OpenInPapers.safariextension/inject.js
+++ OpenInPapers.safariextension/inject.js
@@ -1,36 +1,42 @@
 if (window == window.top)
 {
-    window.addEventListener('keydown', checkKeyDown, false);
-    document.addEventListener('contextmenu', getContextMenuEvent)
+  window.addEventListener('keydown', checkKeyDown, false);
+  safari.self.addEventListener("message", handleMessage, false);
+  document.addEventListener('contextmenu', getContextMenuEvent)
 
+  var settings = {};
 
-    function checkKeyDown(event)
-    {
-    	// <CTRL>+p
-    	if (event.ctrlKey && event.keyCode == 80)
-    	{
-    	    selection = event.view.document.getSelection().toString().trim();
-    	    safari.self.tab.dispatchMessage("openInPapersViaKeyboardShortcut", selection);
-        }
+  function handleMessage(event) {
+    if (event.name == "settingValueIs") {
+      settings[event.message[0]] = event.message[1];
     }
+  }
 
-
-    function getContextMenuEvent(event)
-    {
+  function checkKeyDown(event) {
+    safari.self.tab.dispatchMessage("getSettingValue", "kbshortcut");
+    //console.log(settings);
+    if (event.ctrlKey && event.keyCode == settings.kbshortcut.toUpperCase().charCodeAt(0)) {
         selection = event.view.document.getSelection().toString().trim();
-        
-        userInfo = new Object();
-        
-    	// hasLink: does the right-click target have an HREF attribute?
-    	userInfo.hasLink = !(typeof event.target.href == "undefined"); 
-
-    	// selection: the selected text or the link text, depending on click target
-    	userInfo.selection =  userInfo.hasLink ? event.target.innerText : selection;
-
-    	// href: the link URL if there is one
-    	userInfo.href = userInfo.hasLink ? event.target.href : "";
-
-    	// add userInfo to the event so it'll be seen by the validate function 
-    	safari.self.tab.setContextMenuEventUserInfo(event, userInfo);
+        safari.self.tab.dispatchMessage("openInPapersViaKeyboardShortcut", selection);
     }
+  }
+
+
+  function getContextMenuEvent(event) {
+    selection = event.view.document.getSelection().toString().trim();
+
+    userInfo = new Object();
+
+    // hasLink: does the right-click target have an HREF attribute?
+    userInfo.hasLink = !(typeof event.target.href == "undefined"); 
+
+    // selection: the selected text or the link text, depending on click target
+    userInfo.selection =  userInfo.hasLink ? event.target.innerText : selection;
+
+    // href: the link URL if there is one
+    userInfo.href = userInfo.hasLink ? event.target.href : "";
+
+    // add userInfo to the event so it'll be seen by the validate function 
+    safari.self.tab.setContextMenuEventUserInfo(event, userInfo);
+  }
 }
